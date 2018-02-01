2aa40aa28f915106f5895fc535314c2945436f7c
Sankaran Angamuthu <sankara43@gmail.com>
2018-02-01 10:06:02 +0000

    TEST
    Change-Id: I253c91eda33fc9c1c603a995f2c8bfa1a547e9e7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"myapp/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, MessageToast, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("myapp.controller.App", {
-
+		formatter : formatter,
 		onShowHello : function () {
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
@@ -14,7 +17,35 @@ sap.ui.define([
 
 			// show message
 			MessageToast.show(sMsg);
+		},
+
+		onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext();
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+	
+			oProductDetailPanel.bindElement({ path: sPath });
+			this.byId("productDetailsPanel").setVisible(true); 
+		},
+
+		onFilterProducts : function (oEvent) {
+
+			// build filter array
+			var aFilter = [], sQuery = oEvent.getParameter("query"),
+				// retrieve list control
+				oList = this.getView().byId("productsList"),
+				// get binding for aggregation 'items'
+				oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			// apply filter. an empty filter array simply removes the filter
+			// which will make all entries visible again
+			oBinding.filter(aFilter);
 		}
+
 
 	});
 });
